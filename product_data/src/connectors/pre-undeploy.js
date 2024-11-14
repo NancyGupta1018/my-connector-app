23ff7ff40240324ed6fb76fa2e84ce84ab9133ec
NancyGupta1018 <nancy-gupta@hcl.com>
2024-11-14 12:00:17 +0530

    Initial commit

--- product_data/src/connectors/pre-undeploy.js
+++ product_data/src/connectors/pre-undeploy.js
@@ -1,9 +1,7 @@
 import { createApiRoot } from '../clients/create.client.js';
-import { deleteCTPExtension } from './action.js';
 
 async function preUndeploy() {
   const apiRoot = createApiRoot();
-  await deleteCTPExtension(apiRoot, CTP_TAX_CALCULATOR_EXTENSION_KEY);
 }
 
 async function run() {
@@ -20,15 +18,9 @@ run();
 
 
 import { createApiRoot } from '../clients/create.client.js';
-import { CTP_ORDER_CHANGE_SUBSCRIPTION_KEY } from '../constants/connectors.constants.js';
-import { deleteChangedOrderSubscription } from './actions.js';
 
 async function preUndeploy() {
   const apiRoot = createApiRoot();
-  await deleteChangedOrderSubscription(
-    apiRoot,
-    CTP_ORDER_CHANGE_SUBSCRIPTION_KEY
-  );
 }
 
 async function run() {
