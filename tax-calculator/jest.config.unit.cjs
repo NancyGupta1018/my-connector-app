3cddc7a33f4ff255f350f7428547e8a798bf87eb
NancyGupta1018 <nancy-gupta@hcl.com>
2024-11-14 12:42:40 +0530

    Initial commit

--- tax-calculator/jest.config.unit.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    displayName: 'Tests Javascript Application - Service',
-    moduleDirectories: ['node_modules', 'src'],
-    testMatch: ['**/tests/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
-    testEnvironment: 'node',
-    verbose: true,
-    silent: true,
-};
--- tax-calculator/src/routes/tax.calculator.route.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-import { taxHandler } from '../controllers/tax.calculator.controller.js';
-
-const taxCalculatorRouter = Router();
-
-taxCalculatorRouter.post('/taxCalculator', taxHandler);
-
-export default taxCalculatorRouter;
