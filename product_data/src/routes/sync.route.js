6515804ca9dc380c2f1e7c4b70bab98dda93d9c9
NancyGupta1018 <nancy-gupta@hcl.com>
2024-11-18 13:08:26 +0530

    Initial commit

--- product_data/src/routes/sync.route.js
+++ product_data/src/routes/sync.route.js
@@ -1,20 +1,10 @@
 import { Router } from 'express';
 
-import { syncHandler, getProducts, createProducts, createHello } from '../controllers/sync.controller.js';
+import { createHello } from '../controllers/sync.controller.js';
 
 const syncRouter = Router();
 
-// syncRouter.post('/:storeKey', syncHandler);
-
-// syncRouter.post('/:productId', getProducts);
-
-syncRouter.post('/create', createProducts);
-
 syncRouter.get('/hello', createHello);
-
-
-
-
 
 
 
